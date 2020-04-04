069c82d1e910260d4c5d287739596c5f4d07bc97
hsiangsun <hsiangsun1997@gmail.com>
2020-04-04 10:21:48 +0800

    项目结构初始化

---
+++ mall-coupon/src/test/java/cn/hsiangsun/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hsiangsun.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
